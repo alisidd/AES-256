ea45f98c35e2e4708e7952eb20fec8203ca47aad
Cole Boothman <coleboothman@w134-87-181-145.wireless.uvic.ca>
2016-10-09 11:51:05 -0700

    args input

--- AES.java
+++ AES.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class AES {
     
   static int[] sbox = {0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -110,5 +113,20 @@ public class AES {
   }
 
   public static void main(String[] args) {
+
+        String option = args[0];
+        File key_file = new File(args[1]);
+        File input_file = new File(args[2]);
+
+        if option == "d" {
+
+        }
+
+        if option == "e" {
+            
+        }
+    
+
+
   }
 }
